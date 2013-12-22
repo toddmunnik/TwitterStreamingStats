741a3cc70835625509e2784f7384716f95fbb5c5
toddmunnik <toddmunnik@hotmail.com>
2013-12-22 16:07:02 -0600

    Completed stats calculations

---
+++ TwitterStreamingStats/ATweet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace TwitterStreamingStats
+{
+    [DataContract]
+    public class ATweet
+    {
+        [DataMember] 
+        public string text;
+
+        [DataMember]
+        public entities entities;
+    }
+
+    [DataContract]
+    public class entities
+    {
+        [DataMember]
+        public List<hashtag> hashtags;
+
+        [DataMember]
+        public List<media> media;
+    }
+
+    [DataContract]
+    public class hashtag
+    {
+        [DataMember]
+        public string text;
+
+        [DataMember]
+        public List<int> indices;
+    }
+
+    [DataContract]
+    public class media
+    {
+        [DataMember]
+        public string id;
+        [DataMember]
+        public string id_str;
+        [DataMember]
+        public string media_url;
+        [DataMember]
+        public string media_url_https;
+        [DataMember]
+        public string url;
+        [DataMember]
+        public string display_url;
+        [DataMember]
+        public string expanded_url;
+        [DataMember]
+        public string type;
+    }
+}
--- TwitterStreamingStats/Program.cs
+++ TwitterStreamingStats/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Concurrent;
 using System.Net;
+using System.Runtime.Serialization.Json;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
@@ -16,18 +17,35 @@ namespace TwitterStreamingStats
 {
     class Program
     {
+        private static string trackvalue = String.Empty;
         private static BlockingCollection<string> _queuedTweets;
         private static int totalNumber = 0;
         private static int previousTotalNumber = 0;
-        //top hashtags
-        //percent that contain url
-        //percent that contain a photo
-        //top domain of urls in tweets
+        private static int containsUrl = 0;
+        private static int containsPhotoUrl = 0;
+        private static Dictionary<string, int> topDomains = new Dictionary<string, int>();
+        private static Dictionary<string, int> topHashtags = new Dictionary<string, int>();
 
         static void Main(string[] args)
         {
             try
             {
+                //trackvalue provided in command-line arguments
+                if (args.Count() == 2)
+                {
+                    if (args[0] == "/track")
+                    {
+                        trackvalue = args[1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad Command-line argument: Allowed argument = /track");
+                    }
+                }
+                else
+                {
+                    trackvalue = ConfigurationManager.AppSettings["defaulttrackvalue"];
+                }
                 StreamTwitter();
             }
             catch (Exception e)
@@ -56,7 +74,7 @@ namespace TwitterStreamingStats
             // will cause authentication errors.
 
             var baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
-            "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&track=twitter";
+            "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&track={6}";
 
             var baseString = string.Format(baseFormat,
             OAuthConsumerKey,
@@ -64,7 +82,8 @@ namespace TwitterStreamingStats
             OAuthSignatureMethod,
             OAuthTimestamp,
             OAuthToken,
-            OAuthVersion
+            OAuthVersion,
+            trackvalue
             );
 
             baseString = string.Concat("GET&", Uri.EscapeDataString(ResourceUrl), "&", Uri.EscapeDataString(baseString));
@@ -102,8 +121,7 @@ namespace TwitterStreamingStats
             // Now build the actual request
 
             ServicePointManager.Expect100Continue = false;
-            var postBody = "track=twitter";
-            ResourceUrl += "?" + postBody;
+            ResourceUrl += "?track=" + trackvalue;
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ResourceUrl);
             request.Headers.Add("Authorization", authHeader);
             request.Method = "GET";
@@ -139,6 +157,59 @@ namespace TwitterStreamingStats
                 _queuedTweets.TryTake(out value, Timeout.Infinite);
                 totalNumber += 1;
 
+                //serialize Json string into ATweet
+                var serializer = new DataContractJsonSerializer(typeof(ATweet));
+                var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
+                var rootObject = serializer.ReadObject(stream);
+                stream.Close();
+                var tweet = ((ATweet)rootObject);
+
+                if (tweet.text != null && tweet.text.Contains("http"))
+                    containsUrl += 1;
+
+                //contains a url?
+                if (tweet != null && tweet.entities != null && tweet.entities.media != null)
+                {
+                    if (tweet.entities.media.Count(m => m.url != null || m.display_url != null || m.expanded_url != null || m.media_url != null) > 0)
+                    {
+                        //is the Url a photoUrl?
+                        if (tweet.entities.media.Count(m => m.url.Contains("instagram")
+                            || m.display_url.Contains("pic.twitter.com")) > 0)
+                        {
+                            containsPhotoUrl += 1;
+                        }
+
+                        //top domains       
+                        foreach (media m in tweet.entities.media)
+                        {
+                            if (m.url != null)
+                            {
+                                CheckTopDomains(m.url);
+                            }
+                            if (m.display_url != null)
+                            {
+                                CheckTopDomains(m.display_url);
+                            }
+                            if (m.expanded_url != null)
+                            {
+                                CheckTopDomains(m.expanded_url);
+                            }
+                            if (m.media_url != null)
+                            {
+                                CheckTopDomains(m.media_url);
+                            }
+                        }
+                    }
+
+                    //hashtags
+                    if (tweet.entities.hashtags != null)
+                    {
+                        foreach (hashtag hashtag in tweet.entities.hashtags)
+                        {
+                            CheckTopHashtags(hashtag.text);
+                        }
+                    }
+                }
             }
         }
 
@@ -162,12 +233,110 @@ namespace TwitterStreamingStats
                 Console.WriteLine(string.Format("Number of Tweets Per Hour: {0}", tweetspersecond * 3600));   
             }
 
+            //urls
+            if (totalNumber > 0)
+            {
+                Console.WriteLine(string.Format("Contains a Url: {0}%", string.Format("{0:0.00}",(containsUrl / (float)totalNumber) * 100)));
+                Console.WriteLine(string.Format("Contains a Photo Url: {0}%", string.Format("{0:0.00}",(containsPhotoUrl / (float)totalNumber) * 100)));                 
+            }
+
+            //domains
+            if (topDomains.Count > 0)
+            {
+                List<KeyValuePair<string, int>> listToSort = topDomains.ToList();
+
+                Sort(listToSort);
+
+                //show top 5 domains
+                for (int i = 0; i < 5; i++)
+                {
+                    if (listToSort.Count >= i + 1)
+                        Console.WriteLine(string.Format("Top Domains #{0}: {1} ({2})", i + 1, listToSort[i].Key, listToSort[i].Value)); 
+                }
+            }
+
+            //hashtags
+            if (topHashtags.Count > 0)
+            {
+                List<KeyValuePair<string, int>> listToSort = topHashtags.ToList();
+
+                Sort(listToSort);
+
+                //show top 10 hashtags
+                for (int i = 0; i < 10; i++)
+                {
+                    if (listToSort.Count >= i + 1)
+                        Console.WriteLine(string.Format("Top Hashtags #{0}: #{1} ({2})", i + 1, listToSort[i].Key, listToSort[i].Value));
+                }
+            }
+
             Console.WriteLine("\r\n");
 
             previousTotalNumber = totalNumber;
 
             Thread.Sleep(displayResultsInterval * 1000);
             DisplayResults();
+        }
+
+        private static void Sort(List<KeyValuePair<string, int>> list)
+        {
+            list.Sort((firstPair, nextPair) =>
+            {
+                return nextPair.Value.CompareTo(firstPair.Value);
+            });           
+        }
+
+        static void CheckTopDomains(string url)
+        {
+            int oldValue;
+
+            //some urls are missing the leading protocol
+            if (!url.StartsWith("http"))
+                url = "http://" + url;
+
+            try
+            {
+                Uri uri = new Uri(url);
+                if (topDomains.ContainsKey(uri.Host))
+                {
+                    //topDomains already holds this domain, update the count of the entry +1
+                    topDomains.TryGetValue(uri.Host, out oldValue);
+                    topDomains[uri.Host] = oldValue+1;
+                }
+                else
+                {
+                    //new entry
+                    topDomains.Add(uri.Host, 1);
+                }
+            }
+            catch (Exception e)
+            {
+                //ignore badly formatted url
+            }
+        }
+
+        static void CheckTopHashtags(string hashtag)
+        {
+            int oldValue;
+
+            try
+            {
+                if (topHashtags.ContainsKey(hashtag))
+                {
+                    //topHashtags already holds this hashtag, update the count of the entry +1
+                    topHashtags.TryGetValue(hashtag, out oldValue);
+                    topHashtags[hashtag] = oldValue+1;
+                }
+                else
+                {
+                    //new entry
+                    topHashtags.Add(hashtag, 1);
+                }
+            }
+            catch (Exception e)
+            {
+                //ignore for now
+            }
         }
     }
 }
